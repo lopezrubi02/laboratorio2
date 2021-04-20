45677d6a50ac3f35193dc901a8be7a99cd8aa049
Rubi Polanco <a20183238@pucp.edu.pe>
2021-04-20 03:43:21 -0500

    actvidad/proyecto

--- src/main/java/com/example/laboratorio2/controller/ActividadController.java
+++ src/main/java/com/example/laboratorio2/controller/ActividadController.java
@@ -1,8 +1,15 @@
 package com.example.laboratorio2.controller;
 
+import com.example.laboratorio2.entity.ActividadEntity;
+import com.example.laboratorio2.entity.ProyectoEntity;
 import com.example.laboratorio2.repository.ActividadRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class ActividadController {
@@ -10,5 +17,32 @@ public class ActividadController {
     @Autowired
     ActividadRepository actividadRepository;
 
+
+    @GetMapping("/actividad/editar")
+    public String editarActividad(@RequestParam("id") int id, Model model){
+
+        Optional<ActividadEntity> actividadOpt = actividadRepository.findById(id);
+
+        if(actividadOpt.isPresent()){
+            ActividadEntity actividad = actividadOpt.get();
+            model.addAttribute("actividad",actividad);
+
+            return "actividad/editarActividad";
+        }else{
+            return "redirect:/actividad/editar?id=" + id ;
+        }
+    }
+
+    @GetMapping("/actividad/borrar")
+    public String borrarActividad(@RequestParam("id") int id, Model model){
+        Optional<ActividadEntity> actividadOpt = actividadRepository.findById(id);
+        if(actividadOpt.isPresent()){
+            ActividadEntity actividad = actividadOpt.get();
+            actividadRepository.deleteById(id);
+        }
+        return "redirect:/actividad/editar?id=" + id ;
+
+
+    }
 
 }
--- src/main/java/com/example/laboratorio2/controller/ProyectoController.java
+++ src/main/java/com/example/laboratorio2/controller/ProyectoController.java
@@ -1,10 +1,15 @@
 package com.example.laboratorio2.controller;
 
+import com.example.laboratorio2.entity.ProyectoEntity;
+import com.example.laboratorio2.repository.ActividadRepository;
 import com.example.laboratorio2.repository.ProyectoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 public class ProyectoController {
@@ -12,11 +17,30 @@ public class ProyectoController {
     @Autowired
     ProyectoRepository proyectoRepository;
 
+    @Autowired
+    ActividadRepository actividadRepository;
+
     @GetMapping("/proyecto")
-    public String proyectoLista(Model model){
+    public String listarProyecto(Model model){
 
         model.addAttribute("listaProyectos",proyectoRepository.findAll());
 
         return "listaProyectos";
     }
+
+    @GetMapping("/proyecto/editar")
+    public String editarProyecto(@RequestParam("id") int id, Model model){
+        Optional<ProyectoEntity> proyectoOpt = proyectoRepository.findById(id);
+
+        if(proyectoOpt.isPresent()){
+            ProyectoEntity proyecto = proyectoOpt.get();
+            model.addAttribute("proyecto",proyecto);
+            model.addAttribute("listaActividades",actividadRepository.findAll());
+            return "editarProyecto";
+        }else{
+            return "redirect:/proyecto";
+        }
+    }
+
+
 }
